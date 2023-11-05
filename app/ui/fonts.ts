9b0011c93d59c380bcf639ba238e88eb929dcf96
Frank Arias <32846100+frankliworld@users.noreply.github.com>
2023-11-04 21:42:40 -0500

    add font

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,2 +1,3 @@
-import {Rubik} from "next/font/google";
+import {Rubik, Lusitana} from "next/font/google";
 export const rubik = Rubik({subsets: ["latin"]});
+export const lusitana = Lusitana({subsets: ["latin"], weight: ['400', '700']});
